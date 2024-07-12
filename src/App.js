4cbb659ecb393af47d1d87e8545de920afba7ddf
Gloria-Nyaga <nyagagakiigloria@gmail.com>
2024-07-12 21:57:37 +0300

    made changes

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import About from './Podcast';
 import Landingpage from "./LandingPage";
 import Navbar from "./Navbar";
+import Blogs from "./Blogs";
+import Blogstwo from "./Blogstwo";
 import Portfolio from './Portfolio';
-// import Footer from './Footer';
+import Footer from './Footer';
 import Videos from './Videos';
 
 function App() {
@@ -14,15 +16,17 @@ function App() {
       <div>
         <Navbar/>
        
-
         <Routes>
           <Route path="/" element={<Landingpage />}/>
           <Route path="/podcast" element={<About/>}/>
            <Route path="/videos" element={<Videos />} />
+           <Route path='/blogstwo' element={<Blogstwo/> }/>
+           <Route path='/blogs' element={<Blogs/> }/>
            <Route path='/portfolio' element={<Portfolio/> }/>
 
-
         </Routes>
+
+        {/* <Portfolio/> */}
          {/* <Footer/> */}
       </div>
 
